2285ee5880e7ad58e1c726a636259dadd2f58ab7
Hyeongseop Shin <hyeongsh@c2r5s4.42seoul.kr>
2023-10-11 14:17:15 +0900

    12

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,50 +6,33 @@
 /*   By: hyeongsh <hyeongsh@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 21:17:51 by hyeongsh          #+#    #+#             */
-/*   Updated: 2023/10/10 12:15:39 by hyeongsh         ###   ########.fr       */
+/*   Updated: 2023/10/11 14:14:07 by hyeongsh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_addlst(t_list *lst, t_list *head,
-		void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
+	t_list	*head;
 	t_list	*cur;
 	void	*tempcon;
 
+	if (lst == 0)
+		return (0);
+	head = 0;
 	while (lst != NULL)
 	{
 		tempcon = f(lst->content);
 		cur = ft_lstnew(tempcon);
-		if (cur == 0 && tempcon != 0)
+		ft_lstadd_back(&head, cur);
+		if (cur == 0)
 		{
 			del(tempcon);
 			ft_lstclear(&head, del);
 			return (0);
 		}
-		ft_lstadd_back(&head, cur);
 		lst = lst->next;
 	}
-	return (1);
-}
-
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-{
-	t_list	*head;
-	void	*tempcon;
-
-	if (lst == 0)
-		return (0);
-	tempcon = f(lst->content);
-	head = ft_lstnew(tempcon);
-	if (head == 0 && tempcon != 0)
-	{
-		del(tempcon);
-		return (0);
-	}
-	lst = lst->next;
-	if (ft_addlst(lst, head, f, del) == 0)
-		return (0);
 	return (head);
 }
